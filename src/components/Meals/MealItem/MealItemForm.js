24026e72c6d0bf388355faffdd6ce1557c7dcf5a
alatincic95 <ante.latincic954@gmail.com>
2022-01-20 10:35:23 +0100

    React estoran

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,47 @@
+import React, { useRef, useState } from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+
+const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountinputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountinputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountinputRef}
+        label="Količina"
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Dodaj</button>
+      {!amountIsValid && <p>Molimo unesite valjanu količinu (1-5)</p>}
+    </form>
+  );
+};
+
+export default MealItemForm;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Vršimo dostavu!</h2>
+      <p>Izaberite Vaše omiljeno jelo iz ponude</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
